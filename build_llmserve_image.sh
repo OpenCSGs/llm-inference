9a2bf34cee0ed41c4fde89c840a9f132da442c49
Sean <38311363@qq.com>
2024-05-07 15:15:29 +0800

    update version

--- build_llmserve_image.sh
+++ build_llmserve_image.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 GIT_COMMIT=`git rev-parse HEAD | cut -b 1-12`
 
 DOCKER_REPO="${LLMSERVE_DOCKER_REPO:-registry.cn-beijing.aliyuncs.com/opencsg_public/llmray}"
-VERSION="0.0.2"
+VERSION="0.1.0"
 DOCKER_TAG="$DOCKER_REPO:$VERSION-$GIT_COMMIT"
 DOCKER_FILE="${LLMSERVE_DOCKER_FILE:-deploy/ray/Dockerfile}"
 
